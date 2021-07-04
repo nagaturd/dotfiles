2b9e4d2b94f0cd327edf5c4f356c53b15cc55184
Brian Kwon <thebriankwon@gmail.com>
2021-07-03 20:22:16 -0400

    update

--- nvim/lua/lsp/lua.lua
+++ nvim/lua/lsp/lua.lua
@@ -2,28 +2,24 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
-return require('lua-dev').setup({
-  library = { vimruntime = true, types = true, plugins = true },
-  lspconfig = {
-    settings = {
-      Lua = {
-        runtime = {
-          -- LuaJIT in the case of Neovim
-          version = 'LuaJIT',
-          path = runtime_path
-        },
-        diagnostics = {
-          -- Get the language server to recognize the `vim` global
-          globals = { 'vim' }
-        },
-        workspace = {
-          library = {
-            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true
-          }
+return {
+  settings = {
+    Lua = {
+      runtime = {
+        -- LuaJIT in the case of Neovim
+        version = 'LuaJIT',
+        path = runtime_path
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' }
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true
         }
       }
     }
-
   }
-})
+}
--- nvim/lua/plugins/kommentary.lua
+++ nvim/lua/plugins/kommentary.lua
@@ -1,9 +1,3 @@
-require('kommentary.config').configure_language('default', {
-  hook_function = function()
-    require('ts_context_commentstring.internal').update_commentstring()
-  end
-})
-
 -- Rust comments
 require('kommentary.config').configure_language('rust', {
   prefer_single_line_comments = true
