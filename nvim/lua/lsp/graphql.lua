cd2a5264f4944a17fe273e567d898385748dc6db
Brian Kwon <thebriankwon@gmail.com>
2021-07-05 03:33:07 -0400

    added graphql language server

--- nvim/lua/lsp/graphql.lua
+++ nvim/lua/lsp/graphql.lua
@@ -1,1 +1,8 @@
-return { filetypes = { 'graphql' } }
+return {
+  cmd = {
+    vim.fn.stdpath('data') ..
+        '/lspinstall/graphql/node_modules/.bin/graphql-lsp', 'server', '-m',
+    'stream'
+  },
+  filetypes = { 'graphql' }
+}
