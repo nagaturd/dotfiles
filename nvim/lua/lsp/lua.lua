0d04fb8a83b1fa912710049e9feb6dff3129b2fd
Brian Kwon <thebriankwon@gmail.com>
2021-07-05 03:33:57 -0400

    updated lua language server config

--- nvim/lua/lsp/lua.lua
+++ nvim/lua/lsp/lua.lua
@@ -1,8 +1,12 @@
+local sumneko_root_path = vim.fn.stdpath('data') .. '/lspinstall/lua'
+local sumneko_binary = sumneko_root_path .. '/sumneko-lua-language-server'
+
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
 return {
+  cmd = { sumneko_binary, '-E', sumneko_root_path .. '/main.lua' },
   settings = {
     Lua = {
       runtime = {
@@ -19,7 +23,8 @@ return {
           [vim.fn.expand('$VIMRUNTIME/lua')] = true,
           [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true
         }
-      }
+      },
+      telemetry = { enable = false }
     }
   }
 }
