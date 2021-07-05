6c67a94fc771a2c3378d7a9eebd3ab3d36895591
Brian Kwon <thebriankwon@gmail.com>
2021-07-05 03:32:17 -0400

    added bash language server

--- nvim/lua/lsp/bash.lua
+++ nvim/lua/lsp/bash.lua
@@ -1,1 +1,7 @@
-return { filetypes = { 'sh', 'zsh' } }
+return {
+  cmd = {
+    vim.fn.stdpath('data') ..
+        '/lspinstall/bash/node_modules/.bin/bash-language-server', 'start'
+  },
+  filetypes = { 'sh', 'zsh' }
+}
