ffbe2c80104efade813c661253225a901eb89de4
Brian Kwon <thebriankwon@gmail.com>
2021-07-05 03:31:26 -0400

    updated require()'s

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,9 +4,11 @@ require('options') -- neovim options
 require('mappings') -- keymappings
 
 require('plugins')
+require('plugins.lspsaga')
+require('plugins.lspinstall')
+require('plugins.lsp-rooter')
 require('plugins.nvimtree')
 require('plugins.telescope')
-require('plugins.lsp-rooter')
 require('plugins.tokyonight')
 require('plugins.compe')
 require('plugins.treesitter')
@@ -14,7 +16,8 @@ require('plugins.kommentary')
 require('plugins.symbols')
 require('plugins.galaxyline')
 require('plugins.colorizer')
-require('plugins.pears')
-require('plugins.git')
+require('plugins.autopairs')
+require('plugins.autotags')
+require('plugins.gitsigns')
 
 require('lsp') -- LSP
