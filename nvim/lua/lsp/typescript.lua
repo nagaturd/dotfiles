da8ec677e4bf95ea5239446374a1595fcca31f68
Brian Kwon <thebriankwon@gmail.com>
2021-07-05 03:34:15 -0400

    added typescript language server

--- nvim/lua/lsp/typescript.lua
+++ nvim/lua/lsp/typescript.lua
@@ -1,1 +1,13 @@
-return {}
+return {
+  cmd = {
+    vim.fn.stdpath('data') ..
+        '/lspinstall/typescript/node_modules/.bin/typescript-language-server',
+    '--stdio'
+  },
+  filetypes = {
+    'javascript', 'javascriptreact', 'javascript.jsx', 'typescript',
+    'typescriptreact', 'typescript.tsx'
+  },
+  settings = { documentFormatting = true }
+}
+
