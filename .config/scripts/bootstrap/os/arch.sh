2d9c5ba00193d376a2f190d62ac090b9d21b2562
Brian Kwon <thebriankwon@gmail.com>
2021-07-10 16:20:40 -0400

    fixed bootstrap script

--- .config/scripts/bootstrap/os/arch.sh
+++ .config/scripts/bootstrap/os/arch.sh
@@ -111,9 +111,9 @@ disable_sudo_nopasswd() {
 bootstrap_yadm() {
   print_msg "Installing yadm..."
   sudo -u "${username}" yay -S --needed --noconfirm yadm
+  sudo -Hu "${username}" yadm clone --bootstrap "${DOTFILES_REPO}"
   yadm update-index --assume-unchanged "${XDG_CONFIG_HOME}/yadm/bootstrap" \
     "${HOME}/README.md" "${HOME}/LICENSE"
-  sudo -Hu "${username}" yadm clone --bootstrap "${DOTFILES_REPO}"
 }
 
 finalize() {
