0484de6b25478239656f7ba4928fcc35e011be12
Brian Kwon <thebriankwon@gmail.com>
2021-07-14 15:58:26 -0400

    update

--- .config/scripts/bootstrap/os/arch.sh
+++ .config/scripts/bootstrap/os/arch.sh
@@ -120,7 +120,7 @@ disable_sudo_nopasswd() {
 bootstrap_yadm() {
   print_msg "Installing yadm..."
   sudo -u "${username}" yay -S --needed --noconfirm yadm
-  sudo USERNAME="${username}" -u "${username}" yadm clone --bootstrap "${DOTFILES_REPO}"
+  sudo -u "${username}" yadm clone --bootstrap "${DOTFILES_REPO}"
 }
 
 finalize() {
