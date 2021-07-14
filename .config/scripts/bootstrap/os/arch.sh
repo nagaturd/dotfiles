1a6facaf66c9b542ee827892002c9eb8a4bc6970
Brian Kwon <thebriankwon@gmail.com>
2021-07-14 19:36:33 -0400

    update

--- .config/scripts/bootstrap/os/arch.sh
+++ .config/scripts/bootstrap/os/arch.sh
@@ -121,6 +121,9 @@ bootstrap_yadm() {
   print_msg "Installing yadm..."
   sudo -u "${username}" yay -S --needed --noconfirm yadm
   sudo -Hu "${username}" yadm clone --bootstrap "${DOTFILES_REPO}"
+  sudo -Hu "${username}" yadm update-index --assume-unchanged \
+    "${HOME}/.config/yadm/bootstrap" "${HOME}/README.md" \
+    "${HOME}/LICENSE" "${HOME}/.config/zsh/zsh-aliases"
 }
 
 finalize() {
